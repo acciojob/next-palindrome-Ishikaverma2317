dd68b8c02b7caebbbb7fd787d839d296dc31370f
Ishika Verma <ishikaverma2317@gmail.com>
2025-09-09 16:46:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 function nextPalindrome(num) {
-  //your JS code here.
+    function isPalindrome(n) {
+        let s = n.toString();
+        return s === s.split("").reverse().join("");
+    }
+
+    num++; // next number se start
+    while (!isPalindrome(num)) {
+        num++;
+    }
+    return num;
 }
 
-const input = prompt("Enter a palindrome number");
+const input = parseInt(prompt("Enter a number:"));
 alert(nextPalindrome(input));
